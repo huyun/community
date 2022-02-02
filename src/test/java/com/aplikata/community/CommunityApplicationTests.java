77e5c01008388e1963ea7d81d70f81fe205e7f11
mac <mac@macs-Mac-mini.local>
2022-02-02 09:12:55 +0100

    init repo

---
+++ src/test/java/com/aplikata/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplikata.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
